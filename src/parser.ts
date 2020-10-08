ff278454cbeac41b5c167a06206f5e0de9f006c7
VladislavPetyukevich <pet.vladislav@gmail.com>
2020-10-08 21:46:14 +0300

    Parse assignments and expressions

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,51 @@
 import { TokenType, Token } from './tokenizer';
 
+interface Expression {
+  leftOperand: Token;
+  operator: null | Token;
+  rightOperand: null | Expression;
+}
+
+interface Assignment {
+  variableName: string;
+  value: Expression;
+}
+
+const expectTokenType = (tokenType: TokenType, expectedTokenTypes: TokenType[]) => {
+  if (expectedTokenTypes.includes(tokenType)) {
+    return true;
+  }
+
+  throw new Error('Unxpected token type');
+};
+
+const parseExpression = (tokens: Token[]): Expression => {
+  if (tokens.length === 1) {
+    return {
+      leftOperand: tokens[0],
+      operator: null,
+      rightOperand: null
+    }
+  };
+
+  const rightTokens = tokens.slice(2, tokens.length);
+  return {
+    leftOperand: tokens[0],
+    operator: tokens[1],
+    rightOperand: parseExpression(rightTokens)
+  };
+};
+
+const parseAssignment = (tokens: Token[]): Assignment => {
+  expectTokenType(tokens[0].type, [TokenType.Name]);
+  expectTokenType(tokens[1].type, [TokenType.Equal]);
+
+  return {
+    variableName: tokens[0].stringView,
+    value: parseExpression(tokens.slice(2, tokens.length))
+  };
+};
+
 const splitTokensToStatements = (tokens: Token[]) => {
   if (tokens.length === 0) {
     return tokens;
@@ -16,5 +62,7 @@ const splitTokensToStatements = (tokens: Token[]) => {
 export const parse = (tokens: Token[]) => {
   const tokenStatements = splitTokensToStatements(tokens);
   console.log('tokenStatements: ', tokenStatements);
+  const assignments = tokenStatements.map(parseAssignment);
+  console.log('assignments: ', JSON.stringify(assignments));
 };
 
