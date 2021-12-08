edd1a93cd4ee9b31af7b9f5b1133753df2ee522d
VladislavPetyukevich <pet.vladislav@gmail.com>
2021-12-08 21:05:35 +0300

    Read demo source from file

---
+++ demo.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import {
+  tokenize,
+  parse,
+  compileJS,
+} from './src';
+
+const sourceCodeFilePath = path.resolve('./demo.cringeS');
+const sourceCode = fs.readFileSync(sourceCodeFilePath).toString();
+
+const tokens = tokenize(sourceCode);
+console.log('tokens: ', tokens);
+const statements = parse(tokens);
+console.log('statements: ', JSON.stringify(statements));
+const compiledStatements = compileJS(statements);
+console.log('compiledStatements:');
+console.log(compiledStatements);
+console.log('--------');
+console.log('JS code execution result:');
+eval(compiledStatements);
